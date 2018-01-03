b3e6541b9435602c4aa3d7d1960a8bf9bc1eac71
DhruvamSharma <dhruvamssharma@gmail.com>
2018-01-04 00:42:49 +0530

    TreeSet, It works using compareTo, Comparable Interface

---
+++ TreeSet/TreeSetTestDrive.java
@@ -0,0 +1,8 @@
+
+public class TreeSetTestDrive {
+    public static void main(String...args){
+        TreeSetTestDriveHelper helper = new TreeSetTestDriveHelper();
+        helper.inputData();
+        
+    }
+ }
